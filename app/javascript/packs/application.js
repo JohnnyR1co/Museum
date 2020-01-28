75b9bd99ed7bacc1e9cb8a3157d53fdf2481d924
JohnnyR1co <rubydev@qq.com>
2020-01-28 16:34:22 +1000

    fslightbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,17 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("fslightbox")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('./images', true)
+//const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 
 
 import 'bootstrap'
-import './lightbox'
 import './stylesheets/application'
-import './stylesheets/lightbox'
