d91462e73319f1b7ae5dc431f800ce9b56040b4d
JohnnyR1co <rubydev@qq.com>
2020-03-02 09:39:00 +1000

    edit js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("fslightbox")
-require("packs/lity")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,4 +19,3 @@ require("packs/lity")
 
 import 'bootstrap'
 import './stylesheets/application'
-import './stylesheets/lity'
