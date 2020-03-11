aae755c444e36e6a1cf953b130c807febaae98c8
JohnnyR1co <rubydev@qq.com>
2020-03-11 15:29:28 +1000

    fancybox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("fslightbox")
-require("packs/lity")
+require("packs/fancybox")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,4 +19,4 @@ require("packs/lity")
 
 import 'bootstrap'
 import './stylesheets/application'
-import './stylesheets/lity'
+import './stylesheets/fancybox'
